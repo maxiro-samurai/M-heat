0127cfdacb99dd62df5d60f21b417b6fb037cc58
yushilong <1144634366@qq.com>
2025-04-15 00:33:52 +0800

    更改ADC驱动，已实现读取温度

--- Software/esp32/components/adc/adc.c
+++ Software/esp32/components/adc/adc.c
@@ -1,9 +1,9 @@
 #include "adc.h"
 
 
-static adc_channel_t channel[2] = {ADC_CHANNEL_6, ADC_CHANNEL_7};
-static adc_continuous_handle_t handle = NULL;
-static TaskHandle_t s_task_handle;
+// static adc_channel_t channel[2] = {ADC_CHANNEL_6, ADC_CHANNEL_7};
+// static adc_continuous_handle_t handle = NULL;
+// static TaskHandle_t s_task_handle;
 static const char *TAG = "EXAMPLE";
 adc_continuous_item ADC = {
     .adc_buffer_1 = {0},
@@ -18,133 +18,157 @@ adc_continuous_item ADC = {
     .adc_max_temp = 250,
     .adc_max_temp_auto_flg = true,
 };
-static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data)
-{
-    BaseType_t mustYield = pdFALSE;
-    //Notify that ADC continuous driver has done enough number of conversions
-    vTaskNotifyGiveFromISR(s_task_handle, &mustYield);
-
-    return (mustYield == pdTRUE);
-}
-
-static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc_continuous_handle_t *out_handle)
-{
-    adc_continuous_handle_t handle = NULL;     // 持续读取ADC事件句柄
-
-    adc_continuous_handle_cfg_t adc_config = {  //缓冲区配置
-        .max_store_buf_size = 1024,
-        .conv_frame_size = EXAMPLE_READ_LEN,
-    };
-    ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &handle));
-
-    adc_continuous_config_t dig_cfg = {
-        .sample_freq_hz = 20 * 1000,
-        .conv_mode = EXAMPLE_ADC_CONV_MODE,    //单通道转换模式
-        .format = EXAMPLE_ADC_OUTPUT_TYPE,     //DMA输出格式
-    };
-
-    adc_digi_pattern_config_t adc_pattern[SOC_ADC_PATT_LEN_MAX] = {0};
-    dig_cfg.pattern_num = channel_num;
-    for (int i = 0; i < channel_num; i++) {
-        adc_pattern[i].atten = EXAMPLE_ADC_ATTEN;
-        adc_pattern[i].channel = channel[i] & 0x7;
-        adc_pattern[i].unit = EXAMPLE_ADC_UNIT;
-        adc_pattern[i].bit_width = EXAMPLE_ADC_BIT_WIDTH;
-
-        ESP_LOGI(TAG, "adc_pattern[%d].atten is :%"PRIx8, i, adc_pattern[i].atten);
-        ESP_LOGI(TAG, "adc_pattern[%d].channel is :%"PRIx8, i, adc_pattern[i].channel);
-        ESP_LOGI(TAG, "adc_pattern[%d].unit is :%"PRIx8, i, adc_pattern[i].unit);
-    }
-    dig_cfg.adc_pattern = adc_pattern;
-    ESP_ERROR_CHECK(adc_continuous_config(handle, &dig_cfg));
-
-    *out_handle = handle;
-}
 
 
-static void adc_get_value(adc_continuous_item *adc) {
+static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle);
+static void example_adc_calibration_deinit(adc_cali_handle_t handle);
+// static bool IRAM_ATTR s_conv_done_cb(adc_continuous_handle_t handle, const adc_continuous_evt_data_t *edata, void *user_data)
+// {
+//     BaseType_t mustYield = pdFALSE;
+//     //Notify that ADC continuous driver has done enough number of conversions
+//     vTaskNotifyGiveFromISR(s_task_handle, &mustYield);
 
-    uint32_t ret_num = 0;
-    uint8_t result[EXAMPLE_READ_LEN] = {0};
-    esp_err_t ret;
-    uint8_t m = 0;
-    uint8_t n = 0;
-    memset(result, 0xcc, EXAMPLE_READ_LEN);
+//     return (mustYield == pdTRUE);
+// }
 
-    ulTaskNotifyTake(pdTRUE, portMAX_DELAY); //等待通知
+// static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc_continuous_handle_t *out_handle)
+// {
+//     adc_continuous_handle_t handle = NULL;     // 持续读取ADC事件句柄
 
-    while (1) {
-        ret = adc_continuous_read(handle, result, EXAMPLE_READ_LEN, &ret_num, 0);
-        if (ret == ESP_OK) {
-            ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32" bytes", ret, ret_num);
-            for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES) {
-                adc_digi_output_data_t *p = (void*)&result[i];
-                uint32_t chan_num = EXAMPLE_ADC_GET_CHANNEL(p);
-                uint32_t data = EXAMPLE_ADC_GET_DATA(p);
-                
-                if (chan_num == ADC_CHANNEL_6 ){
-                    // ESP_LOGI(TAG, "ADC_CHANNEL_6 data is :%lu", data);
-                    adc->adc_buffer_1[m] = data; //存储ADC值
-                    
-                    m++;
-                    if (m >= 8) {
-                        m = 0;
-                    }
-                } else if (chan_num == ADC_CHANNEL_7 )
-                {
-                    // ESP_LOGI(TAG, "ADC_CHANNEL_7 data is :%lu", data);
-                    adc->adc_buffer_2[n] = data; //存储ADC值
-                    n++;
-                    if (n >= 8) {
-                        n = 0;
-                    }
-                }
-            }
-            //  vTaskDelay(1);
-            } else if (ret == ESP_ERR_TIMEOUT) {
-            
-                break;
-            }
-        }
+//     adc_continuous_handle_cfg_t adc_config = {  //缓冲区配置
+//         .max_store_buf_size = 1024,
+//         .conv_frame_size = EXAMPLE_READ_LEN,
+//     };
+//     ESP_ERROR_CHECK(adc_continuous_new_handle(&adc_config, &handle));
 
-    }
+//     adc_continuous_config_t dig_cfg = {
+//         .sample_freq_hz = 20 * 1000,
+//         .conv_mode = EXAMPLE_ADC_CONV_MODE,    //单通道转换模式
+//         .format = EXAMPLE_ADC_OUTPUT_TYPE,     //DMA输出格式
+//     };
+
+//     adc_digi_pattern_config_t adc_pattern[SOC_ADC_PATT_LEN_MAX] = {0};
+//     dig_cfg.pattern_num = channel_num;
+//     for (int i = 0; i < channel_num; i++) {
+//         adc_pattern[i].atten = EXAMPLE_ADC_ATTEN;
+//         adc_pattern[i].channel = channel[i] & 0x7;
+//         adc_pattern[i].unit = EXAMPLE_ADC_UNIT;
+//         adc_pattern[i].bit_width = EXAMPLE_ADC_BIT_WIDTH;
+
+//         ESP_LOGI(TAG, "adc_pattern[%d].atten is :%"PRIx8, i, adc_pattern[i].atten);
+//         ESP_LOGI(TAG, "adc_pattern[%d].channel is :%"PRIx8, i, adc_pattern[i].channel);
+//         ESP_LOGI(TAG, "adc_pattern[%d].unit is :%"PRIx8, i, adc_pattern[i].unit);
+//     }
+//     dig_cfg.adc_pattern = adc_pattern;
+//     ESP_ERROR_CHECK(adc_continuous_config(handle, &dig_cfg));
+
+//     *out_handle = handle;
+// }
 
 
-static void adc_voltage(adc_continuous_item *adc)
-{
+
+/*
+
+获取电压值
+
+*/
+static void adc_oneshot_get_voltage(adc_oneshot_unit_handle_t adc1_handle,adc_cali_handle_t adc1_cali_chan0_handle ,adc_cali_handle_t adc1_cali_chan1_handle){
     uint8_t i;
-    for (i = 0; i < 8; i++)
+    uint32_t adc_temp1 = 0;
+    uint32_t adc_temp2 = 0;
+    for (i = 0; i < 8; i++) //读8次ADC值 求平均
     {
-        adc->vol_low += adc->adc_buffer_1[i]; //低电平ADC值
-        adc->vol_high += adc->adc_buffer_2[i]; //高电平ADC值
+        
+        
+        adc_oneshot_read(adc1_handle, ADC_CHANNEL_6, &ADC.adc_buffer_1[i]); //存储ADC值
+        // ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, ADC.adc_buffer_1[i]);
+        adc_oneshot_read(adc1_handle, ADC_CHANNEL_7, &ADC.adc_buffer_2[i]); //存储ADC值
+        // ESP_LOGI(TAG, "ADC%d Channel[%d] Raw Data: %d", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN1, adc_raw[0][1]);
+        adc_temp1 += ADC.adc_buffer_1[i]; //低电平ADC值
+        adc_temp2 += ADC.adc_buffer_2[i]; //高电平ADC值
     }
-    adc->vol_low >>= 3; //平均值滤波
-    adc->vol_high >>= 3; //
+    
+    adc_temp1 >>= 3; //平均值滤波
+    adc_temp2 >>= 3; //
+    
+    
+    ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan0_handle, adc_temp1, &ADC.vol_low));
+    ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %lu mV", ADC_UNIT_1 + 1, ADC_CHANNEL_6, ADC.vol_low);
 
-    adc->vol_low = adc->vol_low * 1000 / 4095; //转换为电压值
-    adc->vol_high = adc->vol_high * 1000 / 4095; //转换为电压值
-    ESP_LOGI(TAG, "vol_low is :%lu", adc->vol_low);
-    ESP_LOGI(TAG, "vol_high is :%lu", adc->vol_high);
+
+    ESP_ERROR_CHECK(adc_cali_raw_to_voltage(adc1_cali_chan1_handle, adc_temp2, &ADC.vol_high));
+    ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %lu mV", ADC_UNIT_1 + 1, ADC_CHANNEL_7, ADC.vol_high);
+
+    
+    
 }
 
 
+// static void adc_get_value(adc_continuous_item *adc) {
 
-static void calculate_temperature(adc_continuous_item *adc){
+//     uint32_t ret_num = 0;
+//     uint8_t result[EXAMPLE_READ_LEN] = {0};
+//     esp_err_t ret;
+//     uint8_t m = 0;
+//     uint8_t n = 0;
+//     memset(result, 0xcc, EXAMPLE_READ_LEN);
 
-     // 1. 计算热敏电阻的电压值 (单位：mV)
-     double voltage = (double) adc->vol_low* 1100 / 4095;
-     ESP_LOGI(TAG, "voltage is :%f", voltage);
-     // 2. 计算热敏电阻的电阻值
-     double resistance = 130000 * (1100 / voltage - 1);
-     ESP_LOGI(TAG, "resistance is :%f", resistance);
-     // 3. 使用 Beta 参数公式计算温度 (单位：K)
-     double temperature_kelvin = 1 / (1 / 298.15 + log(resistance / 130000) / 3950);
- 
-     // 4. 转换为摄氏温度
-     double temperature_celsius = temperature_kelvin - 273.15;
-    ESP_LOGI(TAG, "temperature_celsius is :%f", temperature_celsius);
-     
-}
+//     ulTaskNotifyTake(pdTRUE, portMAX_DELAY); //等待通知
+
+//     while (1) {
+//         ret = adc_continuous_read(handle, result, EXAMPLE_READ_LEN, &ret_num, 0);
+//         if (ret == ESP_OK) {
+//             ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32" bytes", ret, ret_num);
+//             for (int i = 0; i < ret_num; i += SOC_ADC_DIGI_RESULT_BYTES) {
+//                 adc_digi_output_data_t *p = (void*)&result[i];
+//                 uint32_t chan_num = EXAMPLE_ADC_GET_CHANNEL(p);
+//                 uint32_t data = EXAMPLE_ADC_GET_DATA(p);
+                
+//                 if (chan_num == ADC_CHANNEL_6 ){
+//                     // ESP_LOGI(TAG, "ADC_CHANNEL_6 data is :%lu", data);
+//                     adc->adc_buffer_1[m] = data; //存储ADC值
+                    
+//                     m++;
+//                     if (m >= 8) {
+//                         m = 0;
+//                     }
+//                 } else if (chan_num == ADC_CHANNEL_7 )
+//                 {
+//                     // ESP_LOGI(TAG, "ADC_CHANNEL_7 data is :%lu", data);
+//                     adc->adc_buffer_2[n] = data; //存储ADC值
+//                     n++;
+//                     if (n >= 8) {
+//                         n = 0;
+//                     }
+//                 }
+//             }
+//             //  vTaskDelay(1);
+//             } else if (ret == ESP_ERR_TIMEOUT) {
+            
+//                 break;
+//             }
+//         }
+
+//     }
+
+
+// static void adc_voltage(adc_continuous_item *adc)
+// {
+//     uint8_t i;
+//     for (i = 0; i < 8; i++)
+//     {
+//         adc->vol_low += adc->adc_buffer_1[i]; //低电平ADC值
+//         adc->vol_high += adc->adc_buffer_2[i]; //高电平ADC值
+//     }
+//     adc->vol_low >>= 3; //平均值滤波
+//     adc->vol_high >>= 3; //
+
+//     adc->vol_low = adc->vol_low * 1000 / 4095; //转换为电压值
+//     adc->vol_high = adc->vol_high * 1000 / 4095; //转换为电压值
+//     ESP_LOGI(TAG, "vol_low is :%lu", adc->vol_low);
+//     ESP_LOGI(TAG, "vol_high is :%lu", adc->vol_high);
+// }
+
 
 
 
@@ -159,7 +183,7 @@ static void adc_temp(adc_continuous_item *adc){
     double buf;
     
 
-    rt = adc->vol_low * 1000 / ((1100 - adc->vol_low) / 13);
+    rt = adc->vol_low * 1000 / ((3300 - adc->vol_low) / 13);
     adc->now_temp = (100 / (log(rt / 10000.0) / 3950 + 1 / 298.15) - 27315) / 100; //转换为温度值
     ESP_LOGI(TAG, "now_temp is :%u", adc->now_temp);
     // if (adc->now_temp < 151)
@@ -197,31 +221,123 @@ static void adc_temp(adc_continuous_item *adc){
    
 }
 
-void adc_continuous_read_task(void *arg)
-{
+// void adc_continuous_read_task(void *arg)
+// {
     
-    // memset(result, 0xcc, EXAMPLE_READ_LEN);
+//     // memset(result, 0xcc, EXAMPLE_READ_LEN);
 
-    s_task_handle = xTaskGetCurrentTaskHandle(); //获取任务句柄
+//     s_task_handle = xTaskGetCurrentTaskHandle(); //获取任务句柄
 
-    continuous_adc_init(channel, sizeof(channel) / sizeof(adc_channel_t), &handle); //初始化ADC
+//     continuous_adc_init(channel, sizeof(channel) / sizeof(adc_channel_t), &handle); //初始化ADC
 
-    adc_continuous_evt_cbs_t cbs = {
-        .on_conv_done = s_conv_done_cb,
-    };   
+//     adc_continuous_evt_cbs_t cbs = {
+//         .on_conv_done = s_conv_done_cb,
+//     };   
 
-    ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &cbs, NULL));//注册中断回调函数
-    ESP_ERROR_CHECK(adc_continuous_start(handle));  //启动ADC
+//     ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &cbs, NULL));//注册中断回调函数
+//     ESP_ERROR_CHECK(adc_continuous_start(handle));  //启动ADC
+
+//     while (1)
+//     {
+
+
+//         // adc_get_value(&ADC); //获取ADC值
+//         // adc_voltage(&ADC); //获取电压值
+//         // // calculate_temperature(&ADC); //计算温度值
+//         // adc_temp(&ADC); //获取温度值
+//         // // adc_voltage(&ADC);
+//         // vTaskDelay(1000); //延时1ms
+        
+//     }
+    
+// }
+
+void adc_oneshot_read_task(void *arg)
+{
+    //-------------ADC1 Init---------------//
+    adc_oneshot_unit_handle_t adc1_handle;
+    adc_oneshot_unit_init_cfg_t init_config1 = {
+        .unit_id = ADC_UNIT_1,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_new_unit(&init_config1, &adc1_handle));
+
+    //-------------ADC1 Config---------------//
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+        .atten = EXAMPLE_ADC_ATTEN,
+    };
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_6, &config));
+    ESP_ERROR_CHECK(adc_oneshot_config_channel(adc1_handle, ADC_CHANNEL_7, &config));
+
+    //-------------ADC1 Calibration Init---------------//
+    adc_cali_handle_t adc1_cali_chan0_handle = NULL;
+    adc_cali_handle_t adc1_cali_chan1_handle = NULL;
+    bool do_calibration1_chan0 = example_adc_calibration_init(ADC_UNIT_1, ADC_CHANNEL_6, EXAMPLE_ADC_ATTEN, &adc1_cali_chan0_handle);
+    bool do_calibration1_chan1 = example_adc_calibration_init(ADC_UNIT_1, ADC_CHANNEL_7, EXAMPLE_ADC_ATTEN, &adc1_cali_chan1_handle);
+
+
+
 
     while (1)
     {
-        adc_get_value(&ADC); //获取ADC值
-        adc_voltage(&ADC); //获取电压值
-        // calculate_temperature(&ADC); //计算温度值
+        adc_oneshot_get_voltage(adc1_handle,adc1_cali_chan0_handle,adc1_cali_chan1_handle); //获取电压值
+        
         adc_temp(&ADC); //获取温度值
-        // adc_voltage(&ADC);
         vTaskDelay(1000); //延时1ms
+         //Tear Down
+        // ESP_ERROR_CHECK(adc_oneshot_del_unit(adc1_handle));
+        // if (do_calibration1_chan0) {
+        //     example_adc_calibration_deinit(adc1_cali_chan0_handle);
+        // }
+        // if (do_calibration1_chan1) {
+        //     example_adc_calibration_deinit(adc1_cali_chan1_handle);
+        // }
         
     }
-    
+
+
+
+}
+
+
+/*---------------------------------------------------------------
+        ADC Calibration
+---------------------------------------------------------------*/
+static bool example_adc_calibration_init(adc_unit_t unit, adc_channel_t channel, adc_atten_t atten, adc_cali_handle_t *out_handle)
+{
+    adc_cali_handle_t handle = NULL;
+    esp_err_t ret = ESP_FAIL;
+    bool calibrated = false;
+
+if (!calibrated) {
+        ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
+        adc_cali_line_fitting_config_t cali_config = {
+            .unit_id = unit,
+            .atten = atten,
+            .bitwidth = ADC_BITWIDTH_DEFAULT,
+        };
+        ret = adc_cali_create_scheme_line_fitting(&cali_config, &handle);
+        if (ret == ESP_OK) {
+            calibrated = true;
+        }
+}
+
+
+    *out_handle = handle;
+    if (ret == ESP_OK) {
+        ESP_LOGI(TAG, "Calibration Success");
+    } else if (ret == ESP_ERR_NOT_SUPPORTED || !calibrated) {
+        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
+    } else {
+        ESP_LOGE(TAG, "Invalid arg or no memory");
+    }
+
+    return calibrated;
+}
+
+static void example_adc_calibration_deinit(adc_cali_handle_t handle)
+{
+    ESP_LOGI(TAG, "deregister %s calibration scheme", "Line Fitting");
+    ESP_ERROR_CHECK(adc_cali_delete_scheme_line_fitting(handle));
+
 }
--- Software/esp32/components/adc/adc.h
+++ Software/esp32/components/adc/adc.h
@@ -16,13 +16,15 @@
 #include "freertos/task.h"
 #include "freertos/semphr.h"
 #include "esp_adc/adc_continuous.h"
-
+#include "esp_adc/adc_oneshot.h"
+#include "esp_adc/adc_cali.h"
+#include "esp_adc/adc_cali_scheme.h"
 
 #define EXAMPLE_ADC_UNIT                    ADC_UNIT_1
 #define _EXAMPLE_ADC_UNIT_STR(unit)         #unit
 #define EXAMPLE_ADC_UNIT_STR(unit)          _EXAMPLE_ADC_UNIT_STR(unit)
 #define EXAMPLE_ADC_CONV_MODE               ADC_CONV_SINGLE_UNIT_1
-#define EXAMPLE_ADC_ATTEN                   ADC_ATTEN_DB_0
+#define EXAMPLE_ADC_ATTEN                   ADC_ATTEN_DB_11
 #define EXAMPLE_ADC_BIT_WIDTH               SOC_ADC_DIGI_MAX_BITWIDTH
 
 #if CONFIG_IDF_TARGET_ESP32 || CONFIG_IDF_TARGET_ESP32S2
@@ -59,5 +61,5 @@ typedef struct adc_continuous_item
 
 
 void adc_continuous_read_task(void *arg);
-
+void adc_oneshot_read_task(void *arg);
 
